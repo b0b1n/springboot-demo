12115f8f68f13d8413334df426491ff23a5c4e1f
sohaib skious <sohaibskious@gmail.com>
2026-08-19 16:24:13 +0100

    #MVN-000 project creation

---
+++ src/main/java/skious/sohaib/projects/springbootdemo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package skious.sohaib.projects.springbootdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; 
+    }
+}
---
+++ src/test/java/skious/sohaib/projects/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package skious.sohaib.projects.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
